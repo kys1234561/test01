2cbd5bb0af87af66f9092782e6ae72b4c8f5c8be
kys <kys199803@1110.com>
2020-12-05 14:37:51 +0800

    commit message

---
+++ test01/test01.cpp
@@ -0,0 +1,17 @@
+﻿// test01.cpp: 定义应用程序的入口点。
+//
+/*
+sadassadada
+dsadasdsad
+*/
+#include "test01.h"//引用头文件。类似python的import，
+//此处是自己写的库，所以用""来导入。
+
+using namespace std;//命名空间，后面课程会讲，类似python的模块
+
+int main()//程序入口，开始执行的地方
+{
+	cout << "Hello CMake。" << endl;
+	system("pause");
+	return 0;
+}
